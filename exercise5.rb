2be8e8d6fb7f72ced0d5809d23996ede150ab426
David <davidbrooks.now@gmail.com>
2014-01-15 00:41:47 -0500

    Ruby Fundamentals Part Two

---
+++ exercise5.rb
@@ -0,0 +1,10 @@
+puts "Please enter the temperature in Farenheit: "
+num = gets.chomp
+
+def temperature(num)
+	celsius = num.to_i
+	(celsius = (F - 32) x 5/9)
+	puts "F.to_i"
+end
+
+
---
+++ exercise7.rb
@@ -0,0 +1,9 @@
+students = {
+	:cohort1 => 34,
+	:corhort2 => 42,
+	:cohort3 => 22
+}
+
+students.each do |x,y|
+	puts "#{x}: #{y}"
+end
