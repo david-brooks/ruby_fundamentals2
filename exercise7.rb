5c014db464432289207571bd6db2c3a7d633cc4d
David <davidbrooks.now@gmail.com>
2014-01-15 16:27:16 -0500

    Ruby Fundamentals2

--- exercise7.rb
+++ exercise7.rb
@@ -1,11 +1,12 @@
-students = {
-	:cohort1 => 34,
-	:cohort2 => 42,
-	:cohort3 => 22,
-}
 def cohort_students(students)
 students.each {|x,y| puts "#{x}: #{y}" }
 end
+
+students = {
+	:cohort1 => 34,
+	:cohort2 => 42,
+	:cohort3 => 22
+}
 
 cohort_students(students)
 
@@ -13,3 +14,16 @@ students[:cohort4] = 43
 
 puts students.keys
 
+students.each { |x,y| puts "The new number of students in #{x} is:  #{y * 1.05}" }
+
+students.each {|x,y| students[x] = y * 1.05 }
+
+puts students
+
+students.delete(:cohort2)
+
+puts students
+
+total_students = 0
+students.each { |x,y| total_students += y}
+puts total_students
