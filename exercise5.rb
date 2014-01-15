46a755501ef747e4948d230591eba2ae15f6af1a
David <davidbrooks.now@gmail.com>
2014-01-15 09:54:33 -0500

    update to exercise

--- exercise5.rb
+++ exercise5.rb
@@ -1,10 +1,11 @@
 puts "Please enter the temperature in Farenheit: "
-num = gets.chomp
 
-def temperature(num)
-	celsius = num.to_i
-	(celsius = (F - 32) x 5/9)
-	puts "F.to_i"
+def temperature
+farenheit = gets.chomp
+celsius = (farenheit.to_i - 32) * 5/9
+puts "#{celsius} celsius"
 end
+
+temperature
 
 
--- exercise7.rb
+++ exercise7.rb
@@ -1,9 +1,16 @@
 students = {
 	:cohort1 => 34,
 	:corhort2 => 42,
-	:cohort3 => 22
+	:cohort3 => 22,
+	:cohort4 => 43
 }
 
 students.each do |x,y|
 	puts "#{x}: #{y}"
+end
+
+students.each do |x,y| puts "#{x}" 
+end
+
+
 end
