b114a13e078eedbae04566b23fc2a945c0cb329d
David <davidbrooks.now@gmail.com>
2014-01-15 12:31:40 -0500

    Ruby Fundamentals2

--- exercise6.rb
+++ exercise6.rb
@@ -1,2 +1,15 @@
+def display_grocery_list(grocery_list)
+	grocery_list.each { |x| puts "* #{x}" }
+end
+
+def grocery_additions(grocery_list)
+	puts "What food do you want to add to the grocery list: "
+food = gets.chomp
+grocery_list.push(food)
+end
+
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-grocery_list.each { |x| puts "* #{x}" }
+display_grocery_list(grocery_list)
+
+grocery_additions(grocery_list)
+display_grocery_list(grocery_list)
