33d334bb2976f19965ce16dceb0cb225e8559730
David <davidbrooks.now@gmail.com>
2014-01-15 13:38:13 -0500

    Ruby Fundamentals 2

--- exercise6.rb
+++ exercise6.rb
@@ -8,8 +8,19 @@ food = gets.chomp
 grocery_list.push(food)
 end
 
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+grocery_list = ["carrots", "toilet paper", "apples", "salmon", "rice"]
 display_grocery_list(grocery_list)
 
 grocery_additions(grocery_list)
 display_grocery_list(grocery_list)
+
+puts "#{grocery_list.length}"
+
+grocery_list.include?('bananas')
+if grocery_list != "bananas"
+	puts "You need to pick up bananas"
+else
+	puts "You don't need to pick up bananas today"
+end
+
+puts grocery_list[1]
